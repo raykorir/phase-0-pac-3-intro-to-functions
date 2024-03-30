f0617f3a6c1acabd63017704edea5630771734fb
raymond <raymond.korir@student.moringaschool.com>
2024-03-30 15:33:52 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Follow along with the examples here
+function sayHello() {
+    console.log("Hello!");
+  }
+  sayHello();
+  function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+
+function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+
+  function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+  sayHelloToLiz();
+  sayHelloToSamip();
+  sayHelloToGuadalupe();
+
+function doSomething(thing) {
+  console.log(thing);
+}
+doSomething("hello raymond,whats wrong with you");
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+sayHelloTo("raymond");
+
+
+function add(x, y) {
+    console.log(1140 + 453)
+  }
+  add();
+
+
+
+  function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+    console.log("I was called!");
+  }
+say();
+
+
+
+
+
+
+
+
+
+
+
+
+  9
